971e743e0d35b6684067c774d5556e39ad42e64a
lindseygilmore <lindseygilmore21@gmail.com>
2017-07-12 22:00:00 -0500

    main.js udpates

--- client/main.js
+++ client/main.js
@@ -1,1 +1,30 @@
-main.js
+$.ajax({
+	method: 'GET',
+	url: 'http://localhost:9393/lion/json',
+	success: function(response){
+		console.log(response);
+		$('#movies').text(response.name);
+		for(i = 0; i < response.favorite_movies.length; i++){
+			$('#movies').append('<li>'+ response.favorite_movies[i] + '</li>')
+		}
+	}
+})
+
+
+
+$('.link').click(function(e){
+	e.preventDefault();
+	var myURL = $(e.target).atter('href')
+
+	$.ajax({
+		method: 'GET',
+		url: myUrl,
+		success: function(response){
+			$('movies').empty();
+			$('movie').text(response.name);
+			for(i = 0; i < response.favorite_movies.length; i++){
+				$('#movies').append('<li>'+ response.favorite_movies[i] + '</li>')
+			}
+		}
+	})
+})
